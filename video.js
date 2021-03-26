117159f67bde3f0cee57772375df05ef36f5f3f6
ryanbeales <ryanbeales@gmail.com>
2021-03-26 15:15:34 -0700

    Add stats and keyboard commands

--- video.js
+++ video.js
@@ -9,12 +9,51 @@ const videocontext = videocanvas.getContext('2d');
 const renderedcanvas = document.getElementById('renderedcanvas');
 const renderedcontext = renderedcanvas.getContext('2d');
 
+// Status overlay
+const statsoverlay = document.getElementById('statsoverlay_background');
+
+
 // Global bodypix net and segment output
 let net = null;
 let segment = null;
-let background = 0;
+
+class OptionsCycle{
+  constructor(values, startingoption=0) {
+      this.values = values
+      this.selected = startingoption
+  }
+
+  next() {
+    this.selected += 1
+    if (this.selected > this.values.length-1) {
+      this.selected = 0
+    }
+  }
+
+  get() {
+    return this.values[this.selected]
+  }
+}
+
+let background = new OptionsCycle([
+    [0,0,0,0], // Transparent
+    1, // Video
+    [0,255,0,255], //Green
+    [0,0,255,255] //blue  
+  ]);
+let multiplier = new OptionsCycle([0.5, 0.75, 1])
+let internalresolution = new OptionsCycle([0.5, 0.75, 1, 0.25])
+
+function display_config() {
+  document.getElementById('statsoverlay-multiplier').innerHTML = multiplier.get()
+  document.getElementById('statsoverlay-internal-resolution').innerHTML = internalresolution.get()
+}
+
 
 // Called each time a frame is available on the incoming webcam video
+let lastvideoframetime = 0
+const statsoverlay_video_fps = document.getElementById('statsoverlay-video-fps')
+
 function renderVideo(now, metadata) {
     // Draw video to video context
     videocontext.drawImage(videoElement,0,0);
@@ -24,42 +63,20 @@ function renderVideo(now, metadata) {
       // Get video frame data
       frame = videocontext.getImageData(0,0,videoElement.width, videoElement.height);
 
-      /* background modes:
-          0 = transparent
-          1 = video
-          2 = green
-          3 = blue
-          4 = red */
-
       // If background is video just avoid this.
-      if (background != 1) { 
-        let background_colour =  [0,0,0,0];
-        switch (background) {
-          case 0: // transparent
-            background_colour = [0,0,0,0];
-            break;
-          case 2: // green
-            background_colour = [0,255,0,255];
-            break;
-          case 3: // blue
-            background_colour = [0,0,255,255];
-            break;
-        }        
-
+      if (background.get() != 1) { 
         // Loop through all segment data and set pixel values based on the value
         for (var i=0; i < segment.data.length; i++) {
           current = segment.data[i]
-          p_left = segment.data[i-1]
-          p_right = segment.data[i+1]
 
           // If segment data is 1, it's part of the person
           if (segment.data[i] == 1) {
             frame.data[i*4+3] = 255
           } else {
-            frame.data[i*4] = background_colour[0]
-            frame.data[i*4+1] = background_colour[1]
-            frame.data[i*4+2] = background_colour[2]
-            frame.data[i*4+3] = background_colour[3]
+            frame.data[i*4] = background.get()[0]
+            frame.data[i*4+1] = background.get()[1]
+            frame.data[i*4+2] = background.get()[2]
+            frame.data[i*4+3] = background.get()[3]
           }
         }
       }
@@ -68,17 +85,29 @@ function renderVideo(now, metadata) {
     }
     // Make sure this function is called on next available frame
     videoElement.requestVideoFrameCallback(renderVideo)
+
+    //FPS calc
+    frameend = performance.now()
+    statsoverlay_video_fps.innerHTML = (1000 / (frameend-lastvideoframetime)).toFixed(0)
+    lastvideoframetime = frameend
 }
 
 // Use bodypix to take the incoming webcam data and run the segmentation
+let lastsegmentframetime = 0
+const statsoverlay_segementation_fps = document.getElementById('statsoverlay-segmentation-fps')
+
 async function createMask(now, metadata) {
   // If bodypix is loaded
   if (net) {
-    segment = await net.segmentPerson(videoElement, {internalResolution: 0.5});
-
-    // Set up callback for next available frame
-    videoElement.requestVideoFrameCallback(createMask)
+    segment = await net.segmentPerson(videoElement, {internalResolution: internalresolution.get()});
   }
+  // Set up callback for next available frame
+  videoElement.requestVideoFrameCallback(createMask)
+
+  // FPS calc
+  frameend = performance.now()
+  statsoverlay_segementation_fps.innerHTML = (1000 / (frameend-lastsegmentframetime)).toFixed(0)
+  lastsegmentframetime = frameend
 }
 
 // Start the webcam on videoElement
@@ -96,11 +125,12 @@ function start_webcam() {
 
 // Load bodypix
 function start_bodypix() {
+      net = null;
       // We can reload this at execution time to change options?
       // https://github.com/tensorflow/tfjs-models/blob/master/body-pix/README.md
       bodyPix.load({
-        //architecture: 'ResNet50',
-        multiplier: 1,
+        architecture: 'MobileNetV1',
+        multiplier: multiplier.get(),
         stride: 16,
         quantBytes: 4
       }).then(function (net2) { net = net2; })  
@@ -121,8 +151,10 @@ videoElement.onloadeddata = () => {
   renderedcanvas.height = videoElement.height;
 
   // Trigger the processing for each incoming frame
-  videoElement.requestVideoFrameCallback(renderVideo)
-  videoElement.requestVideoFrameCallback(createMask)
+  videoElement.requestVideoFrameCallback(renderVideo);
+  videoElement.requestVideoFrameCallback(createMask);
+
+  display_config();
 }
 
 
@@ -131,10 +163,19 @@ window.addEventListener("keyup", event => {
   switch(event.key) {
     // Cycle through backgrounds
     case 'b':
-      background += 1;
-      if (background > 3) {
-        background = 0
-      }
+      background.next();
+      break;
+    case 'i':
+      internalresolution.next();
+      display_config()
+      break;
+    case 'm':
+      multiplier.next();
+      start_bodypix();
+      display_config()
+      break;
+    case 's':
+      statsoverlay.hidden = !statsoverlay.hidden
       break;
   };
 });
